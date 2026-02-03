f3d62d7895d1a9cd3e9fc955d1271adaadd19dbe
Sean Villacampa <villacampasean@gmail.com>
2026-02-03 15:11:27 +0800

    Add files via upload

---
+++ task8 calculator/script.js
@@ -0,0 +1,59 @@
+const num1 = document.getElementById
+
+
+let display = document.getElementById(output);
+
+function outputOne() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "1"
+  }
+
+  function outputTwo() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "2"
+  }
+  function outputThree() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "3"
+  }
+  function outputFour() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "4"
+  }
+  function outputFive() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "5"
+  }
+  function outputSix() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "6"
+  }
+  function outputSeven() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "7"
+  }
+  function outputEight() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "8"
+  }
+  function outputNine() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "9"
+  }
+  function outputPlus() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "+"
+  }
+  function outputMinus() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "-"
+  }
+  function outputMultiply() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "x"
+  }
+  function outputDivide() {
+    let finaldisplay = document.getElementById("output")
+    output.innerHTML = "/"
+  }
+  
